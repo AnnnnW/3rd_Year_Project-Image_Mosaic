a57728c12e8bf260699711c2f84588920cc174d3
Annan Wang <annan.wang@student.manchester.ac.uk>
2018-09-13 14:17:54 +0100

    Output And Exception * create output file and show it up in a new window * throw exception when the path of the file is incorrect and exit the process

--- Image_Mosaic/main.cpp
+++ Image_Mosaic/main.cpp
@@ -12,8 +12,25 @@ using namespace cv;                             //包含cv命名空间
 
 int main()
 {
-    Mat srcImage = imread("/Users/wangannan/Desktop/IMG_6064.jpg");             // Load the image
-    imshow("Original Image", srcImage);         //Show the image, with the window name ""Original Image
-    waitKey(0);                                 // Wait for any key to be pressed
+    Mat srcImage = imread("/Users/wangannan/Desktop/testing_image.jpg");             // Load the image
+    if (!srcImage.data)
+    {
+        printf("Can't read the file, please check the path and try again.\n");
+        return -1;
+    }
+    namedWindow("Original Image");              // name a window and decide the window size
+    imshow("Original Image", srcImage);         //Show the image, with the window name "Original Image"
+    
+    imwrite("Result.jpg", srcImage);            // write an image as an output file
+    Mat resultImage = imread("Result.jpg");     // print out the result image in a new window
+    if (!resultImage.data)
+    {
+        printf("Can't read the file, please check the path and try again.\n");
+        return -1;
+    }
+    namedWindow("Result Image");
+    imshow("Result Image", resultImage);
+    
+    waitKey(0);                                 // Wait for any key to be pressed to exit
     return 0;
 }
