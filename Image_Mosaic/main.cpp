582bc8b784a6768fc44a7c3fab3667b6d71ec549
Annan Wang <annan.wang@student.manchester.ac.uk>
2018-09-20 19:22:09 +0100

    Change the path for the input and output images and resize the srcImg to fit the screen

--- Image_Mosaic/main.cpp
+++ Image_Mosaic/main.cpp
@@ -10,25 +10,31 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;                             //包含cv命名空间
 
+String readpath = "/Users/wangannan/Image_Mosaic/IMG/Input/";
+String savepath = "/Users/wangannan/Image_Mosaic/IMG/Output/";
+
 int main()
 {
-    Mat srcImage = imread("/Users/wangannan/Desktop/testing_image.jpg");             // Load the image
+    Mat srcImage = imread(readpath + "testing_image.jpg");             // Load the image
+    Mat tempImage;
     if (!srcImage.data)
     {
         printf("Can't read the file, please check the path and try again.\n");
         return -1;
     }
-    namedWindow("Original Image", WINDOW_NORMAL);              // name a window and decide the window size
+    namedWindow("Original Image", WINDOW_AUTOSIZE);              // name a window and decide the window size
     imshow("Original Image", srcImage);         //Show the image, with the window name "Original Image"
     
-    imwrite("Result.jpg", srcImage);            // write an image as an output file
+    resize(srcImage,tempImage,Size(100,100),0,0,INTER_LINEAR);      //resize the srcimage to a certain fix size 100 * 100
+    
+    imwrite(savepath + "Result.jpg", tempImage);            // write an image as an output file
     Mat resultImage = imread("Result.jpg");     // print out the result image in a new window
     if (!resultImage.data)
     {
         printf("Can't read the file, please check the path and try again.\n");
         return -1;
     }
-    namedWindow("Result Image", WINDOW_NORMAL);
+    namedWindow("Result Image", WINDOW_AUTOSIZE);
     imshow("Result Image", resultImage);
     
     waitKey(0);                                 // Wait for any key to be pressed to exit
